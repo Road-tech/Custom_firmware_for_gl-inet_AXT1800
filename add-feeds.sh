6e116fb7470ae52b8d3f3acc990c7a61a1baaefa
Road <496814664@qq.com>
2022-07-06 22:18:04 +0800

    Update add-feeds.sh

--- add-feeds.sh
+++ add-feeds.sh
@@ -20,6 +20,8 @@
 git clone https://github.com/vernesong/OpenClash.git $GITHUB_WORKSPACE/OpenClash
 cp -r $GITHUB_WORKSPACE/OpenClash/luci-app-openclash $GITHUB_WORKSPACE/gl-infra-builder/feeds/glinet/luci-app-openclash
 
+git clone https://github.com/jerrykuku/luci-theme-argon.git $GITHUB_WORKSPACE/gl-infra-builder/feeds/glinet/luci-theme-argon
+
 svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-ramfree $GITHUB_WORKSPACE/gl-infra-builder/feeds/glinet/luci-app-ramfree
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-zerotier $GITHUB_WORKSPACE/gl-infra-builder/feeds/glinet/luci-app-zerotier
 svn co https://github.com/coolsnowwolf/packages/trunk/net/zerotier $GITHUB_WORKSPACE/gl-infra-builder/feeds/glinet/zerotier
